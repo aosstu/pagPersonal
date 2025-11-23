c0ea7ae6dbff6abf775bba0bfe07013dedde11a5
ariel <ariel@arroba.com>
2025-11-22 22:33:21 -0300

    socials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}'
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        socials: "#786cbb",
+      },
       backgroundColor: {
-        'green-500-transparent': 'rgba(34, 197, 94, 0.5)', // green-500 con 50% opacidad
-      }
-    }
+        "green-500-transparent": "rgba(34, 197, 94, 0.5)", // green-500 con 50% opacidad
+      },
+    },
   },
   plugins: [],
-}
+};
